d5dbdf381af0972b6bb4e5e33c45c3a4c1c50da2
Santiago Collaud <santicoll_00@yahoo.com.ar>
2025-03-17 19:23:28 -0300

    subiendo archivos app_prueba

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const withPWA = require("next-pwa")({
+  dest: "public",        // Directorio donde se generará el Service Worker
+  disable: process.env.NODE_ENV === "development",
+  register: true,        // Registro automático del Service Worker
+  skipWaiting: true,     // Activa la nueva versión inmediatamente
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,        // Habilita el uso de la carpeta "app"
+  },
+});
+
 export default nextConfig;
